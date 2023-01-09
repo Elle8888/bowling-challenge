c5977c160aab7567cff7845e8c7b87b59095e6db
Elle <lou.d.b.rigodon@gmail.com>
2023-01-09 09:43:33 +0100

    add more test

--- bowlingScoreCard.js
+++ bowlingScoreCard.js
@@ -8,13 +8,13 @@ class BowlingScoreCard {
   calculateScore = () => {
     const maxFrames = 10;
 
-    // Loop over until the 10th frame
+    // Loop over each frame until the 10th frame
     for (let frame = 0; frame < maxFrames; frame++) {
       let frameScore = 0;
 
       frameScore = frame === maxFrames - 1
-      ? this.calculateTenthFrameScore()
-      : this.calculateRegularFrameScore();
+      ? this.calculateLastFrameScore()
+      : this.calculateOtherFrameScore();
 
       this.score += frameScore;
     }
@@ -22,21 +22,23 @@ class BowlingScoreCard {
     return this.getGameResult();
   }
 
-  calculateOtherFrameScore() {
-    // Check for strike
-    if (this.rolls[this.rollIndex] === 10) {
-      this.rollIndex++;
-      return 10 + this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1];
-    }
-    // Check for spare
-    if (this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1] === 10) {
-      this.rollIndex += 2;
-      return 10 + this.rolls[this.rollIndex];
-    }
-    // Otherwise, just add the score of the rolls for this frame
-    this.rollIndex += 2;
-    return this.rolls[this.rollIndex - 2] + this.rolls[this.rollIndex - 1];
+
+calculateOtherFrameScore = () => {
+  // Check for strike
+  if (this.rolls[this.rollIndex] === 10) {
+    this.rollIndex++;
+    return 10 + this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1];
   }
+  // Check for spare
+  if (this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1] === 10) {
+    this.rollIndex += 2;
+    return 10 + this.rolls[this.rollIndex];
+  }
+  // Otherwise, just add the score of the rolls for this frame
+  const score = this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1];
+  this.rollIndex += 2;
+  return score;
+}
 
   calculateLastFrameScore = () => {
     // Check for strike in the 10th frame
@@ -50,9 +52,11 @@ class BowlingScoreCard {
       return 10 + this.rolls[this.rollIndex];
     }
     // Otherwise, just add the score of the three rolls for the 10th frame
+    const score = this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1] + this.rolls[this.rollIndex + 2];
     this.rollIndex += 3;
-    return this.rolls[this.rollIndex - 3] + this.rolls[this.rollIndex - 2] + this.rolls[this.rollIndex - 1];
+    return score;
   }
+  
 
   getGameResult = () => {
     // Check for gutter game
@@ -63,6 +67,18 @@ class BowlingScoreCard {
     if (this.score === 300) {
       return 'Perfect Game';
     }
-    //
+    return this.score;
   }
+
+  // getGameResult() {
+  //   if (this.score === 300) {
+  //     return { score: this.score, result: "strike" };
+  //   } else if (this.score < 300 && this.score % 10 === 0) {
+  //     return { score: this.score, result: "spare" };
+  //   } else {
+  //     return { score: this.score, result: "open" };
+  //   }
+  // }
 }
+
+module.exports = BowlingScoreCard
---
+++ bowlingScoreCard.test.js
@@ -0,0 +1,55 @@
+const BowlingScoreCard = require('./bowlingScoreCard'); // This is the file where the BowlingScoreCard class is defined
+
+it('calculateScore', () => {
+  const rolls = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6];
+  const scoreCard = new BowlingScoreCard(rolls);
+  expect(scoreCard.calculateScore()).toBe(133);
+});
+
+it('calculateScore with gutter game', () => {
+  const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const scoreCard = new BowlingScoreCard(rolls);
+  expect(scoreCard.calculateScore()).toBe('Gutter Game');
+});
+
+it('calculateScore with perfect game', () => {
+  const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+  const scoreCard = new BowlingScoreCard(rolls);
+  expect(scoreCard.calculateScore()).toBe('Perfect Game');
+});
+
+it('calculateScore with strike in the 10th frame', () => {
+  const scoreCard = new BowlingScoreCard([1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 , 1, 2, 10, 10, 10]);
+  const score = scoreCard.calculateScore();
+  expect(score).toBe(57);
+});
+
+it('calculateScore with spare in the 10th frame', () => {
+  const scoreCard = new BowlingScoreCard([1, 2, 3, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 5, 10]);
+  const score = scoreCard.calculateScore();
+  expect(score).toBe(93);
+});
+
+it('calculateScore with the 10th frame that is neither a strike nor spare', () => {
+  const scoreCard = new BowlingScoreCard([1, 2, 3, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 0]);
+  const score = scoreCard.calculateScore();
+  expect(score).toBe(82);
+});
+
+it('calculateScore with multiple spares in a row', () => {
+    const rolls = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
+    const scoreCard = new BowlingScoreCard(rolls);
+    expect(scoreCard.calculateScore()).toBe(150);
+  });
+
+it('calculateScore with all rolls as 1', () => {
+    const rolls = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0];
+    const scoreCard = new BowlingScoreCard(rolls);
+    expect(scoreCard.calculateScore()).toBe(20);
+  });
+
+it('calculateScore with alternating rolls of 1 and 9', () => {
+    const rolls = [1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 9];
+    const scoreCard = new BowlingScoreCard(rolls);
+    expect(scoreCard.calculateScore()).toBe(118);
+  });
